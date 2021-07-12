df7c1743eeddcf3f7c3c63c6c68540e84aed2204
jhseo <wbgus010@gmail.com>
2021-07-13 03:06:42 +0900

    add kakao map api

--- app.py
+++ app.py
@@ -48,12 +48,13 @@
 
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+from urllib.parse import urlencode, unquote
+import requests
+import json
+
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
-db = client.covid
-
-key = "4MZE91nl58LxJryx5TYngBgW4jTxjLlYQlteJ3kTud9MYYFJKO8khFD%2BeeJRqD%2FQGeT8jlwOJVBASEUXoiborw%3D%3D"
-requesturl = "http://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=1&perPage=10&serviceKey=4MZE91nl58LxJryx5TYngBgW4jTxjLlYQlteJ3kTud9MYYFJKO8khFD%2BeeJRqD%2FQGeT8jlwOJVBASEUXoiborw%3D%3D"
+db = client.dpsparta
 
 ## HTML 화면 보여주기
 @app.route('/')
@@ -61,18 +62,53 @@ def homework():
     return render_template('test.html')
 
 # (POST) API
-@app.route('/covid', methods=['POST'])
-def save_order():
-    # sample_receive = request.form['sample_give']
-    # print(sample_receive)
-    return jsonify({'msg': '이 요청은 POST!'})
+@app.route('/center', methods=['GET'])
+def get_center():
+    centers = list(db.center.find({}, {'_id': False}))
+    return jsonify({'result': 'success', 'centers': centers})
 
+# find API
+@app.route('/find', methods=['GET'])
+def find_vaccine():
+    url = 'https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=1000&serviceKey=4MZE91nl58LxJryx5TYngBgW4jTxjLlYQlteJ3kTud9MYYFJKO8khFD%2BeeJRqD%2FQGeT8jlwOJVBASEUXoiborw%3D%3D'
+    response = requests.get(url)
+    r_dict = json.loads(response.text)
+    r_response = r_dict.get("data")
+    id = r_response[0].get("id")
+    data = list(db.center.find({'id': id}))
+    print(data)
+    return jsonify({'what': 'what'})
 
-# 목록보기(Read) API
+# OPEN API
 @app.route('/covid', methods=['GET'])
 def view_vaccine():
-    sample_receive = request.args.get(requesturl+key)
-    return jsonify(sample_receive)
+    url = 'https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=1000&serviceKey=4MZE91nl58LxJryx5TYngBgW4jTxjLlYQlteJ3kTud9MYYFJKO8khFD%2BeeJRqD%2FQGeT8jlwOJVBASEUXoiborw%3D%3D'
+    response = requests.get(url)
+    r_dict = json.loads(response.text)
+    r_response = r_dict.get("data")
+    for res in r_response:
+        id = res.get("id")
+        data = list(db.center.find({'id': id}))
+        if (len(data) == 0):
+            center = {
+                'address' : res.get("address"),
+                'centerName' : res.get("centerName"),
+                'centerType' : res.get("centerType"),
+                'createdAt' :  res.get("createdAt"),
+                'facilityName' : res.get("facilityName"),
+                'id' : res.get("id"),
+                'lat' : res.get("lat"),
+                'lng' : res.get("lng"),
+                'org' :res.get("org"),
+                'phoneNumber' : res.get("phoneNumber"),
+                'sido' : res.get("sido"),
+                'sigungu' : res.get("sigungu"),
+                'updatedAt' : res.get("updatedAt"),
+                'zipCode' : res.get("zipCode")
+            }
+            db.center.insert_one(center)
+
+    return jsonify({'what':'what'})
 
 
 if __name__ == '__main__':
